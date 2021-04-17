9040d2fa339dcb670643d1188d1548e125b7f376
jDan735 <daniel734@bk.ru>
2021-04-17 22:52:27 +0300

    Transform all to string for join

--- sqlfocus/__init__.py
+++ sqlfocus/__init__.py
@@ -1,3 +1,3 @@
 from .table import SQLTable, SQLTableBase
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
--- sqlfocus/helpers.py
+++ sqlfocus/helpers.py
@@ -5,7 +5,7 @@ def sstr(text):
 
 def all2string(args, q="'"):
     if args.__class__ == list or args.__class__ == tuple:
-        return [_ if _.__class__ != str else f"{q}{sstr(_)}{q}" for _ in args]
+        return [f"{_}" if _.__class__ != str else f"{q}{sstr(_)}{q}" for _ in args]
     else:
         return [f"{_}={args[_]}" if args[_].__class__ != str
                 else f"{_}={q}{sstr(args[_])}{q}" for _ in args]
